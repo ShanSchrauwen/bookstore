f6f547a62ee7c0176f6ac224c68401795fec241f
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2022-04-07 14:20:46 +0300

    delete functionality

--- src/main/java/com/haagahelia/controller/BookController.java
+++ src/main/java/com/haagahelia/controller/BookController.java
@@ -3,6 +3,7 @@ package com.haagahelia.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +37,11 @@ public class BookController {
 	public String save(Book book) {
 		repository.save(book);
 		return "redirect:booklist";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 }
